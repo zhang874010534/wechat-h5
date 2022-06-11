b3b75e600440f6addf73fff604e49b2641add5d6
zhangce <z874010534@vip.qq.com>
2022-06-11 14:56:38 +0800

    feat: 项目初始化 h5测试公众号授权

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,19 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <div>
+    2222
+  </div>
   <router-view/>
 </template>
+<script>
 
+import {defineComponent} from "vue";
+
+export  default  defineComponent({
+  mounted() {
+    // window.location.href = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx62ce81cf4b455a2e&redirect_uri=http%3A%2F%2F192.168.31.222%3A8080%2F&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect'
+  }
+})
+</script>
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
---
+++ src/api/index.js
@@ -0,0 +1,6 @@
+export default {
+  wechatRedirect: '/wechat/redirect?',
+  wechatConfig: '/wechat/jssdk',
+  getUserInfo: '/wechat/getUserInfo',
+  payWallet: '/wechat/pay/payWallet'
+}
