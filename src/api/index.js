7663c225e38210162279c294b84df798b313b80c
zhangce <z874010534@vip.qq.com>
2022-06-11 22:18:29 +0800

    feat: history

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,5 @@
 export default {
-  wechatRedirect: '/wechat/redirect?',
+  wechatRedirect: '/wechat/redirect?url=http%3A%2F%2F192.168.31.222%3A8080%2F&scope=snsapi_userinfo',
   wechatConfig: '/wechat/jssdk',
   getUserInfo: '/wechat/getUserInfo',
   payWallet: '/wechat/pay/payWallet'
